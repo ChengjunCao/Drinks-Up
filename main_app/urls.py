66eef3dfb9275a66bf83bd5d139420df20bd1d81
Mike Cao <mikecao369@gmail.com>
2021-01-29 16:54:30 -0500

    upload login_home page

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-      path('', views.home, name='home'),
+    path('', views.index, name='index'),
+    path('home/', views.home, name='home'),
+    path('drinks/create/', views.DrinkCreate.as_view(), name='drinks_create'),
+    path('accounts/signup/', views.signup, name='signup'),
+
 ]
