282871f5b7892a463c1c4b505084f27fc338b518
Mike Cao <mikecao369@gmail.com>
2021-02-04 16:37:12 -0500

    implement authorization

--- main_app/views.py
+++ main_app/views.py
@@ -7,6 +7,8 @@ import uuid
 import boto3
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 import requests
 
 S3_BASE_URL = 'https://s3.us-east-2.amazonaws.com/'
@@ -14,14 +16,17 @@ BUCKET = 'albumcollector'
 def main(request):
   return render(request, 'main.html')
 
+@login_required
 def home(request):
   drinks = Drink.objects.filter(user=request.user)[:3]
   return render(request, 'home.html', {'drinks': drinks})
 
+@login_required
 def drinks_index(request):
   drinks = Drink.objects.filter(user=request.user)
   return render(request, 'drinks/index.html', {'drinks': drinks})
 
+@login_required
 def drinks_detail(request, drink_id):
   drink = Drink.objects.get(id=drink_id)
   url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s={}"
@@ -33,10 +38,12 @@ def drinks_detail(request, drink_id):
     instructions = drink_data['drinks'][0]['strInstructions']
   return render(request, 'drinks/detail.html', {'drink': drink, 'instructions': instructions})
 
+@login_required
 def create_page(request):
   drink_form = DrinkForm()
   return render(request, 'main_app/drink_form.html', {'drink_form': drink_form})
 
+@login_required
 def create_drink(request):
   image_file = request.FILES.get('image-file', None)
   if image_file:
@@ -52,11 +59,12 @@ def create_drink(request):
     except:
       print('An error occurred uploading file to S3')
   
-
+@login_required
 def update_page(request, drink_id):
   drink = Drink.objects.get(id=drink_id)
   return render(request, 'drinks/update.html', {'drink': drink})
 
+@login_required
 def update_drink(request, drink_id):
   drink = Drink.objects.get(id=drink_id)
   drink.user = request.user
@@ -69,10 +77,11 @@ def update_drink(request, drink_id):
   drink.save()
   return redirect('detail', drink_id=drink_id)
 
-class DrinkDelete(DeleteView):
+class DrinkDelete(LoginRequiredMixin, DeleteView):
   model = Drink
   success_url = '/home/'
 
+@login_required
 def signup(request):
   error_message = ''
   if request.method == 'POST':
@@ -87,16 +96,14 @@ def signup(request):
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
 
-
+@login_required
 def search_page(request):
   random = requests.get('https://www.thecocktaildb.com/api/json/v1/1/random.php').json()
   random_drink = random['drinks'][0]
-  print(random_drink)
   return render(request, 'drinks/search_page.html', {'random_drink': random_drink})
 
-
+@login_required
 def search_result(request):
   search = request.POST['drink_name']
   results = Drink.objects.filter(name__icontains=search)
-  print(results)
   return render(request, 'drinks/result.html', {'results': results})
